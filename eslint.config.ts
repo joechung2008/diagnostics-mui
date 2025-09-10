4bd7e8e43475f11669bf50d047881174de43f272
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-10 15:36:54 -0400

    Remove globals package

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,6 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import reactPlugin from "eslint-plugin-react-x";
 import { globalIgnores } from "eslint/config";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default [
@@ -26,7 +25,6 @@ export default [
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
       parserOptions: {
         project: [
           "./tsconfig.json",
